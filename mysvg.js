393518b825397feccc901d29cd37e4c2484a176d
Nate Tryon <ematrygames@gmail.com>
2022-01-23 11:46:07 -0500

    New page, coordinates

--- mysvg.js
+++ mysvg.js
@@ -11,29 +11,27 @@ var viewCenterY = viewbox_Y + (viewbox_height / 2);
 
 var zoomLevel = 10;
 var gridlines = [];
+var gridLinesOn = true;
 var mouseX = 0;
 var mouseY = 0;
+var mousePositionDisplay = true;
 
 window.onload = () => {
-    document.body.onkeydown = keyDownJS;
+    document.body.onkeydown = keyDown;
     document.body.onwheel = mouseWheel;
-    document.getElementById("gridButton").onclick = drawGridLines;
     document.getElementById("svgMap").onmousemove = mousePosition;
+    document.getElementById("gridToggle").onclick = toggleGrid;
+    document.getElementById("mousePosToggle").onclick = toggleMousePosition;
     setViewbox();
 }
 
 function drawGridLines() {
 
     const map = document.getElementById("svgMap");
+    const xcoords = document.getElementById("xcoords");
+    const ycoords = document.getElementById("ycoords");
 
-    for (var i = 0; i < gridlines.length; ++i)
-    {
-        var el = document.getElementById(gridlines[i]);
-        if (el != null) map.removeChild(el);
-    }
-
-    while (gridlines.length > 0)
-        gridlines.pop();
+    removeGridLines();
 
     for (var i = viewbox_X; i < viewbox_X + viewbox_width; i += viewbox_width/12)
     {
@@ -48,15 +46,11 @@ function drawGridLines() {
         gridlines.push("gridX_" + i);
         map.appendChild(el);
         
-        var txtEl = document.createElementNS(SVG_NAMESPACE, "text");
-        txtEl.setAttribute("id", "gridtX_" + i);
-        txtEl.setAttribute("x", i);
-        txtEl.setAttribute("y", viewbox_Y + 10);
-        txtEl.setAttribute("fill", "#ffffff");
-        txtEl.innerHTML = "Bye!";
-        
-        gridlines.push("gridtX_" + i);
-        map.appendChild(txtEl);
+        var coordEl = document.createElement("div");
+        coordEl.setAttribute("id", "coordtX_" + i);
+        coordEl.setAttribute("class", "x-coordinate");
+        coordEl.innerText = i;        
+        xcoords.appendChild(coordEl);
     }
     
     for (var j = viewbox_Y; j < viewbox_Y + viewbox_height; j += viewbox_height/9)
@@ -71,12 +65,18 @@ function drawGridLines() {
         
         gridlines.push("gridY_" + j);
         map.appendChild(el);
+
+        var coordEl = document.createElement("div");
+        coordEl.setAttribute("id", "coordtY_" + i);
+        coordEl.setAttribute("class", "y-coordinate");
+        coordEl.innerText = j;        
+        ycoords.appendChild(coordEl);
     }
 
 }
 
 
-function keyDownJS(event) {
+function keyDown(event) {
 
     switch (event.keyCode)
     {
@@ -116,10 +116,47 @@ function mouseWheel(event) {
 
 function mousePosition(event) {
 
+    if (!mousePositionDisplay)
+        return;
+
     mouseX = Math.floor(viewbox_X + event.offsetX * (viewbox_width / MAP_WIDTH));
     mouseY = Math.floor(viewbox_Y + event.offsetY * (viewbox_height / MAP_HEIGHT));
-    console.log("mouseX: " + mouseX);
-    console.log("mouseY: " + mouseY);
+
+    var xShift = (mouseX <= viewbox_X + 0.9 * viewbox_width) ? (mouseX + 10 * (zoomLevel / 10)) : (mouseX - 80 * (zoomLevel / 10));
+    var yShift = (mouseY >= viewbox_Y + 0.05 * viewbox_height) ? (mouseY - 5 * (zoomLevel / 10)) : (mouseY + 20 * (zoomLevel / 10));
+
+    var mpEl = document.getElementById("mousePosText");
+
+    mpEl.setAttribute("x", xShift);
+    mpEl.setAttribute("y", yShift);
+    mpEl.setAttribute("class", "mouse-pos-text zoom" + zoomLevel);
+    mpEl.setAttribute("visibility", "visible");
+    mpEl.innerHTML = "(" + mouseX + ", " + mouseY + ")";
+    // console.log("mouseX: " + mouseX);
+    // console.log("mouseY: " + mouseY);
+}
+
+function removeGridLines() {
+
+    const map = document.getElementById("svgMap");
+    const xcoords = document.getElementById("xcoords");
+    const ycoords = document.getElementById("ycoords");
+
+    while (xcoords.firstChild)
+        xcoords.removeChild(xcoords.firstChild);
+    
+    while (ycoords.firstChild)
+        ycoords.removeChild(ycoords.firstChild);
+
+    for (var i = 0; i < gridlines.length; ++i)
+    {
+        var el = document.getElementById(gridlines[i]);
+        if (el != null) map.removeChild(el);
+    }
+
+    while (gridlines.length > 0)
+        gridlines.pop();
+
 }
 
 function resetViewbox() {
@@ -141,7 +178,12 @@ function setViewbox() {
     map.setAttribute("viewBox", `${viewbox_X} ${viewbox_Y} ${viewbox_width} ${viewbox_height}`);
     viewCenterX = viewbox_X + (viewbox_width / 2);
     viewCenterY = viewbox_Y + (viewbox_height / 2);
-    drawGridLines();
+
+    if (gridLinesOn)
+        drawGridLines();
+
+    var mpEl = document.getElementById("mousePosText");
+        mpEl.setAttribute("visibility", "hidden");
 
 }
 
@@ -169,6 +211,34 @@ function shiftView(direction) {
     }
 
     setViewbox();
+}
+
+function toggleGrid() {
+
+    if (gridLinesOn)
+    {
+        removeGridLines();
+        gridLinesOn = false;
+    }
+
+    else
+    {
+        drawGridLines();
+        gridLinesOn = true;
+    }
+}
+
+function toggleMousePosition() {
+
+    if (mousePositionDisplay)
+    {
+        var mpEl = document.getElementById("mousePosText");
+        mpEl.setAttribute("visibility", "hidden");
+        mousePositionDisplay = false;
+    }
+
+    else
+        mousePositionDisplay = true;
 }
 
 function zoom(zoomIn) {
